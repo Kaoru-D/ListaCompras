6c2603cfff8b5ce3221a07ab993cedeb9b030b24
Daniel-K <daniel.rt5d@gmail.com>
2026-04-01 11:35:07 -0500

    feat: implement local storage functionality for items management

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,37 @@ import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
+
+const getLocalStorage = () => {
+  const list = localStorage.getItem('items');
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+}
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
   const addItem = (itemName) => {
     const newItem = {
       name:itemName,
       completed:false,
       id: nanoid(),
     };
-    setItems([...items,newItem]);
+    const newItems = [...items,newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   }
   
   const removeItem = (itemId)=> {
-    setItems(items.filter((item)=> item.id !== itemId));
+    const newItems = items.filter((item)=> item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
   }
 
 
